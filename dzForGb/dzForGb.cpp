97a2e763bf8c410776d432e10c319063f4e1ed99
ArtZetD <56314346+ArtZetD@users.noreply.github.com>
2021-11-01 14:59:28 +0300

    вроде сделал но только 1 номер

--- dzForGb/dzForGb.cpp
+++ dzForGb/dzForGb.cpp
@@ -4,9 +4,43 @@
 #include <algorithm>
 #include <list>
 #include <numeric>
+#include <map>
+#include <set>
 
+template <class T>
+void insertSorted(std::vector<T>& vec, T value)
+{
+	auto const it = std::lower_bound(vec.begin(), vec.end(), value);
+	if (!it)
+		vec.push_back(value);
+	else vec.insert(it, value);
+}
+template<class T>
+void insertSorted(std::list<T>& vec, T value)
+{
+	auto const it = std::lower_bound(vec.begin(), vec.end(), value);
+	if (!it)
+		vec.push_back(value);
+	else vec.insert(it, value);
+}
+template<class T, class U>
+void insertSorted(std::map<T, U>& vec, T value)
+{
+	auto const it = std::lower_bound(vec.begin(), vec.end(), value);
+	if (!it)
+		vec.push_back(value);
+	else vec.insert(it, value);
+}
+template<class T>
+void insertSorted(std::set<T>& vec, T value)
+{
+	auto const it = std::lower_bound(vec.begin(), vec.end(), value);
+	if (!it)
+		vec.push_back(value);
+	else vec.insert(it, value);
+}
 
 int main()
 {
-  
+
 }
